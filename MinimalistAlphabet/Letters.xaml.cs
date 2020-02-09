ae6d2a350fdb3df92f7836342adeeaac24945bf9
Brock <33275496+BrockNicholas@users.noreply.github.com>
2020-02-09 01:19:15 -0500

    Initial development

---
+++ MinimalistAlphabet/Letters.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MinimalistAlphabet
+{
+    public partial class Letters : ContentPage
+    {
+        public Letters(string which)
+        {
+            InitializeComponent();
+            
+            switch (which)
+            {
+                case "Upper":
+                    Symbol.Text = "A";
+                    break;
+                case "Lower":
+                    Symbol.Text = "a";
+                    break;
+                case "Numbers":
+                    Symbol.Text = "1";
+                    break;
+            }
+
+        }
+
+        private void OnSwipedLeft(object sender, SwipedEventArgs e)
+        {
+            Symbol.Text = Symbol.Text;
+        }
+
+        private void OnSwipedRight(object sender, SwipedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MinimalistAlphabet/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MinimalistAlphabet
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Upper.Clicked += Upper_Clicked;
+            Lower.Clicked += Lower_Clicked;
+            Numbers.Clicked += Numbers_Clicked;
+        }
+
+        private void Upper_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Letters("Upper"));
+        }
+
+        private void Lower_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new NavigationPage(new Letters("Lower")));
+        }
+
+        private void Numbers_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Letters("Numbers"));
+        }        
+
+        
+    }
+}
