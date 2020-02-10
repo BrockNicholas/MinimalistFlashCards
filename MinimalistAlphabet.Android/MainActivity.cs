7b54a6c43846d0b7ede898b22b98ba3d6b2a6fcc
Brock Weigel <brock.weigel.ctr@navy.mil>
2020-02-09 20:47:29 -0500

    Changed Droid app name

--- MinimalistAlphabet.Android/MainActivity.cs
+++ MinimalistAlphabet.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MinimalistAlphabet.Droid
 {
-    [Activity(Label = "MinimalistAlphabet", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "AlphaCards", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
